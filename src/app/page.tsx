10de9c64c052815deec6599c2cca545fbebd1024
Mohd Arham <mdarham2405@gmail.com>
2025-07-22 01:55:59 +0530

    Blur rect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
         <h1>TAGLINE</h1>
         <p className="content-text">Some more content for this page <br />INFO AND DESCRIPTION</p>
       </section>
+      <div className="flex justify-center gap-10 p-8"style={{ gap: '200px' }}>
+        <div className="box blur-box w-180 h-100"></div>
+        <div className="box blur-box w-180 h-100"></div>
+      </div>
     </div>
   );
 }
