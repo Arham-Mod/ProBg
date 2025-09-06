539361538df0827827cfbaf406053a89b975c8b6
Mohd Arham <mdarham2405@gmail.com>
2025-09-06 15:00:00 +0530

    font link added for global support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import Navbar from './components/Navbar';
 import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html>
+      <head>
+          <link href='https://fonts.googleapis.com/css?family=Unbounded' rel='stylesheet' />
+        </head>
       <body>
         <Navbar />
         <main>{children}</main>
